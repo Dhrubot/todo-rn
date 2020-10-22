e8e24f25496dd8d58007450942d4977403ffc15c
Dhrubot <dhrubo.dh@gmail.com>
2020-10-22 18:53:03 -0400

    Added a input box and a button and started with some basi stylesheet options

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 40}}>
-      <View>
-        <TextInput placeholder='Things TO DO'/>
-        <Button title='ADD'/>
+    <View style={styles.mainContainer}>
+      <View style={styles.inputContainer} >
+        <TextInput
+          placeholder='Things TO DO'
+          style={styles.inputArea}
+        />
+        <Button title='ADD' />
       </View>
       <View>
 
@@ -16,5 +19,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  mainContainer: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputArea: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }
 });
