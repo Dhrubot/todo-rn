16860f917aa52e2861c1efc6b71c53fb00a7d87f
Dhrubot <dhrubo.dh@gmail.com>
2020-11-23 20:24:38 -0500

    Added styles

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [todoTask, setTodoTask] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  const handleInput = enteredText => {
+    setTodoTask(enteredText)
+  }
+
+  const handleAdd = () => {
+    setTodoList([...todoList, todoTask])
+  }
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.inputContainer} >
         <TextInput
           placeholder='Things TO DO'
           style={styles.inputArea}
+          onChangeText={ handleInput }
+          value={ todoTask } 
         />
-        <Button title='ADD' />
+        <Button title='ADD' onPress={ handleAdd }/>
       </View>
-      <View>
-
-      </View>
+      <ScrollView>
+        { todoList.map(todo => <View key={todo} style={ styles.todoList }><Text> {todo} </Text></View> ) }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   mainContainer: {
     padding: 50
   },
@@ -28,9 +42,17 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   inputArea: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
     padding: 10
+  },
+  todoList: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
+  
 });
